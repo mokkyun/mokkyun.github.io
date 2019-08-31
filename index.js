73574032b3acbbb36773e8b8c8db4c6e771c1141
Mo <mohamedjama53@gmail.com>
2019-08-31 22:35:12 +0100

    add Intersection Observer for navbar

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 $(function() {
-	$('a').bind('click',function(event){
-		var $anchor = $(this);
+  $("a").bind("click", function(event) {
+    var $anchor = $(this);
 
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
-		}, 1500,'easeInOutExpo');
-		event.preventDefault();
-	});
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr("href")).offset().top
+        },
+        1500,
+        "easeInOutExpo"
+      );
+    event.preventDefault();
+  });
 });
+
+//Intersection Observer API for changing navbar on scroll
+
+const header = document.querySelector("header");
+const hero = document.querySelector("#hero");
+const heroOptions = {
+  rootMargin: "-400px 0px 0px 0px"
+};
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scroll");
+    } else {
+      header.classList.remove("nav-scroll");
+    }
+  });
+}, heroOptions);
+
+observer.observe(hero);
